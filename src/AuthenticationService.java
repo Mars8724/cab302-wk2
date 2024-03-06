38db0efa40e1d710b98c43c62f69340b0b035fba
Linh Nguyen <146163331+ln-egg@users.noreply.github.com>
2024-03-06 17:22:42 +1000

    Added AuthenticationService.java Implemented AuthenticationService constructor Implemented signUp method Implemented logIn method Changed authService to new AuthenticationService(users) Changed onSignUp to check if signUp returns a user or null

--- src/AuthenticationService.java
+++ src/AuthenticationService.java
@@ -3,12 +3,9 @@ import java.util.Objects;
 
 public class AuthenticationService implements IAuthenticationService {
     private ArrayList<User> users;
-
-    // TODO Now: Add a constructor to initialize the users list with the default user
-    public AuthenticationService() {
-        this.users = new ArrayList<User>();
+    public AuthenticationService(ArrayList<User> users) {
+        this.users = users;
     }
-    // TODO Now: Implement the signUp method to add a new user to the list if the username is not taken and return the user; returns null otherwise
     public User signUp(String username, String password) {
         for (User user : users) {
             if (Objects.equals(user.getUsername(), username)) return null;
@@ -17,7 +14,6 @@ public class AuthenticationService implements IAuthenticationService {
         users.add(newUser);
         return newUser;
     }
-    // TODO Now: Implement the logIn method to return the user if the username and password match, and null otherwise
     public User logIn(String username, String password) {
         for (User user : users) {
             if (Objects.equals(user.getUsername(), username) && Objects.equals(user.getPassword(), password)) return user;
